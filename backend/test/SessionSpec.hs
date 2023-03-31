69cd9a8b580e50ae328cfa7d72e4d821709e4250
Andre Pereira <apereira@meetsoci.com>
2023-03-31 19:11:17 -0300

    Session tests that could fail because of seconds difference between the time of the session and the time got by the test routine

--- backend/test/SessionSpec.hs
+++ backend/test/SessionSpec.hs
@@ -4,11 +4,31 @@ module SessionSpec
     ( suiteSpec
     ) where
 
+import qualified Test.HUnit.Lang as HunitLang
+import Data.CallStack
+import Control.Exception as E
+import Control.DeepSeq
+
 import Test.Hspec
 import Database.MySQL.Simple
 import qualified Model.User as User
 import Data.Time.Clock.POSIX
 import Model.Session
+import Test.QuickCheck.State (State(expected))
+
+shouldBeWithValidationFunction :: (a -> a -> IO Bool) -> String -> a -> a -> Expectation
+shouldBeWithValidationFunction f errorMessage actual expected = assertEqual f errorMessage expected actual
+
+assertEqual :: (a -> a -> IO Bool) -> String -> a -> a -> HunitLang.Assertion
+assertEqual f errorMessage expected actual = do
+  comparisonResult <- f actual expected
+  if comparisonResult then return ()
+  else (errorMessage `deepseq` E.throwIO (HunitLang.HUnitFailure location $ HunitLang.ExpectedButGot (Just "") errorMessage "False"))
+
+location :: HasCallStack => Maybe SrcLoc
+location = case reverse callStack of
+  (_, loc) : _ -> Just loc
+  [] -> Nothing
 
 suiteSpec :: Connection -> SpecWith ()
 suiteSpec dbConn = do
@@ -17,18 +37,30 @@ suiteSpec dbConn = do
     it "saveSession should save" $ do
       _ <- execute dbConn "TRUNCATE TABLE sessions" ()
       currentTimestamp <- getPOSIXTime
-      saveSession dbConn (User.User 1 "name" "email" "password") >>= (`shouldBe` Session { sessionId = 1, userId = 1, expiration = round currentTimestamp + 60 })
+      shouldBeWithValidationFunction (\actual expected -> do
+                                                actualValue <- actual
+                                                expectedValue <- expected
+                                                return (abs ((expiration actualValue) - (expiration expectedValue)) <= 5)
+        ) "Sessions with expiration date within a 11 seconds interval" (saveSession dbConn (User.User 1 "name" "email" "password")) (return Session { sessionId = 1, userId = 1, expiration = round currentTimestamp + 60 })
 
     it "getActiveSession should return session" $ do
       currentTimestamp <- getPOSIXTime
-      getActiveSession dbConn 1 >>= (`shouldBe` Session { sessionId = 1, userId = 1, expiration = round currentTimestamp + 60 })
+      shouldBeWithValidationFunction (\actual expected -> do
+                                                actualValue <- actual
+                                                expectedValue <- expected
+                                                return (abs ((expiration actualValue) - (expiration expectedValue)) <= 5)
+        ) "Sessions with expiration date within a 11 seconds interval" (getActiveSession dbConn 1) (return Session { sessionId = 1, userId = 1, expiration = round currentTimestamp + 60 })
 
     it "getActiveSession should return session not found" $ do
       getActiveSession dbConn 2 >>= (`shouldBe` SessionNotFound)
 
     it "renewSession should return session with new expiration date" $ do
       currentTimestamp <- getPOSIXTime
-      renewSession dbConn (Session { sessionId = 1, userId = 1, expiration = 0 }) >>= (`shouldBe` Session { sessionId = 1, userId = 1, expiration = round currentTimestamp + 60 })
+      shouldBeWithValidationFunction (\actual expected -> do
+                                                actualValue <- actual
+                                                expectedValue <- expected
+                                                return (abs ((expiration actualValue) - (expiration expectedValue)) <= 5)
+        ) "Sessions with expiration date within a 11 seconds interval" (renewSession dbConn (Session { sessionId = 1, userId = 1, expiration = 0 })) (return Session { sessionId = 1, userId = 1, expiration = round currentTimestamp + 60 })
 
     it "renewSession should return session not found" $ do
       renewSession dbConn (Session { sessionId = 2, userId = 1, expiration = 0 }) >>= (`shouldBe` SessionNotFound)
