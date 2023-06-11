2b01b97b1a8f0eed75dacaed052cc0570b5d563c
Andre Pereira <apereira@meetsoci.com>
2023-06-11 20:51:53 -0300

    refactor of HandlerCommons

--- backend/src/Handlers/HandlerCommons.hs
+++ backend/src/Handlers/HandlerCommons.hs
@@ -14,9 +14,6 @@ import Web.Scotty.Internal.Types
 import qualified Data.Aeson as Aeson
 import qualified Data.Text.Lazy as TL
 
-import qualified Network.Wai.Parse
-import qualified Data.ByteString.Lazy.UTF8 as BSU
-
 import Database.MySQL.Simple
 import Control.Monad.IO.Class
 import qualified Security.Jwt as Jwt
@@ -34,65 +31,49 @@ handleJsonRequest errorHandler successHandler = do
 
 handleLoggedJsonRequest :: Aeson.FromJSON t => String -> Int -> Connection -> String -> ActionT TL.Text IO b -> ActionT TL.Text IO b -> (t -> Session.Session -> ActionT TL.Text IO b) -> ActionT TL.Text IO b
 handleLoggedJsonRequest secret sessionTime conn requiredPermission invalidJsonErrorHandler invalidTokenErrorHandler successHandler = do
-    authorizationHeader <- header $ TL.pack "Authorization"
-    case authorizationHeader of
-        Nothing -> invalidTokenErrorHandler
-        Just headerContents -> do
-            let token = TL.replace "Bearer " "" headerContents
-            let tokenSession = Jwt.decodeSession secret (TL.unpack token)
-            case tokenSession of
-                Session.SessionNotFound -> invalidTokenErrorHandler
-                sessionFromToken -> do
-                    session <- liftIO (Session.getActiveSession conn (Session.userId sessionFromToken))
-                    case session of
-                        Session.SessionNotFound -> invalidTokenErrorHandler
-                        session' -> do
-                            user <- liftIO (User.getUserById conn (Session.userId session'))
-                            permissions <- liftIO (Permission.getUserPermissions conn (User.userId user))
-                            (if any (\permission -> Permission.permission permission == requiredPermission) permissions then (do
-                                requestBody <- body
-                                let maybeJson = Aeson.decode requestBody
-                                case maybeJson of
-                                    Nothing -> invalidJsonErrorHandler
-                                    Just json' -> liftIO (Session.renewSession conn sessionTime session') >> successHandler json' session') else invalidTokenErrorHandler)
+    session <- getSession secret sessionTime conn requiredPermission
+    case session of
+        Session.SessionNotFound -> invalidTokenErrorHandler
+        session' -> do
+            requestBody <- body
+            let maybeJson = Aeson.decode requestBody
+            case maybeJson of
+                Nothing -> invalidJsonErrorHandler
+                Just json' -> successHandler json' session'
 
 handleLoggedRequest :: String -> Int -> Connection -> String -> ActionT TL.Text IO b -> (Session.Session -> ActionT TL.Text IO b) -> ActionT TL.Text IO b
 handleLoggedRequest secret sessionTime conn requiredPermission invalidTokenErrorHandler successHandler = do
+    session <- getSession secret sessionTime conn requiredPermission
+    case session of
+        Session.SessionNotFound -> invalidTokenErrorHandler
+        session' -> successHandler session'
+    
+
+handleLoggedFilesRequest :: String -> Int -> Connection -> String -> ActionT TL.Text IO b -> ([File] -> Session.Session -> ActionT TL.Text IO b) -> ActionT TL.Text IO b 
+handleLoggedFilesRequest secret sessionTime conn requiredPermission invalidTokenErrorHandler successHandler = do
+    session <- getSession secret sessionTime conn requiredPermission
+    case session of
+        Session.SessionNotFound -> invalidTokenErrorHandler
+        session' -> do
+            requestFiles <- files
+            successHandler requestFiles session'
+
+getSession :: String -> Int -> Connection -> String -> ActionT TL.Text IO Session.Session
+getSession secret sessionTime conn requiredPermission = do
     authorizationHeader <- header $ TL.pack "Authorization"
     case authorizationHeader of
-        Nothing -> invalidTokenErrorHandler
+        Nothing -> return Session.SessionNotFound
         Just headerContents -> do
             let token = TL.replace "Bearer " "" headerContents
             let tokenSession = Jwt.decodeSession secret (TL.unpack token)
             case tokenSession of
-                Session.SessionNotFound -> invalidTokenErrorHandler
+                Session.SessionNotFound -> return Session.SessionNotFound
                 sessionFromToken -> do
                     session <- liftIO (Session.getActiveSession conn (Session.userId sessionFromToken))
                     case session of
-                        Session.SessionNotFound -> invalidTokenErrorHandler
+                        Session.SessionNotFound -> return Session.SessionNotFound
                         session' -> do
                             user <- liftIO (User.getUserById conn (Session.userId session'))
                             permissions <- liftIO (Permission.getUserPermissions conn (User.userId user))
                             (if any (\permission -> Permission.permission permission == requiredPermission) permissions then 
-                                liftIO (Session.renewSession conn sessionTime session') >> successHandler session' else invalidTokenErrorHandler)
-
-handleLoggedFilesRequest :: String -> Int -> Connection -> String -> ActionT TL.Text IO b -> ([File] -> Session.Session -> ActionT TL.Text IO b) -> ActionT TL.Text IO b 
-handleLoggedFilesRequest secret sessionTime conn requiredPermission invalidTokenErrorHandler successHandler = do
-    authorizationHeader <- header $ TL.pack "Authorization"
-    case authorizationHeader of
-        Nothing -> invalidTokenErrorHandler
-        Just headerContents -> do
-            let token = TL.replace "Bearer " "" headerContents
-            let tokenSession = Jwt.decodeSession secret (TL.unpack token)
-            case tokenSession of
-                Session.SessionNotFound -> invalidTokenErrorHandler
-                sessionFromToken -> do
-                    session <- liftIO (Session.getActiveSession conn (Session.userId sessionFromToken))
-                    case session of
-                        Session.SessionNotFound -> invalidTokenErrorHandler
-                        session' -> do
-                            user <- liftIO (User.getUserById conn (Session.userId session'))
-                            permissions <- liftIO (Permission.getUserPermissions conn (User.userId user))
-                            (if any (\permission -> Permission.permission permission == requiredPermission) permissions then (do
-                                requestFiles <- files
-                                liftIO (Session.renewSession conn sessionTime session') >> successHandler requestFiles session') else invalidTokenErrorHandler)
+                                liftIO (Session.renewSession conn sessionTime session') >> return session' else return Session.SessionNotFound)
